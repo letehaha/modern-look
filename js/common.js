2d2cb064a5cfd76f9ac3ed84d8eda58acd3b76b9
dmitry <letehaha@gmail.com>
2016-05-01 13:56:45 +0300

    section folio

--- js/common.js
+++ js/common.js
@@ -2,4 +2,8 @@ $(function(){
 
 	$("a[href*='#']").mPageScroll2id();
 
+	$(".list_mix li").click(function() {
+		$(".list_mix li").removeClass("active").animate();
+		$(this).addClass("active").animate();
+	});
 });
