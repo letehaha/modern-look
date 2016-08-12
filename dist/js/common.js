69fa94f3dff76f49f04921c2ba6bdfe5bd6ddb31
letehaha <letehaha@gmail.com>
2016-08-12 16:49:54 +0300

    update

---
+++ dist/js/common.js
@@ -0,0 +1,10 @@
+$(function(){
+
+	$("a[href*='#']").mPageScroll2id();
+
+	$(".list_mix li").click(function() {
+		$(".list_mix li").removeClass("active").animate();
+		$(this).addClass("active").animate();
+	});
+
+});
